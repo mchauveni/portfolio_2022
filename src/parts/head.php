2bc74d0c51eca61f122eb73702063994ef712f89
Chaosbeing <matheorigami@gmail.com>
2022-12-14 10:37:22 +0100

    matomo local

--- src/parts/head.php
+++ src/parts/head.php
@@ -12,21 +12,22 @@
 <link rel="preload" href="/assets/fonts/Righteous.ttf" as="font" type="font/ttf" crossorigin />
 
 <script src="https://cdn.counter.dev/script.js" data-id="84cc6a26-b513-44b7-8020-719e2d147afe" data-utcoffset="1" defer></script>
+
 <!-- Matomo -->
-<script defer>
+<script>
     var _paq = window._paq = window._paq || [];
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-        var u = "https://mchauveni.matomo.cloud/";
+        var u = "//mchauveni.alwaysdata.net/IUT/matomo/";
         _paq.push(['setTrackerUrl', u + 'matomo.php']);
         _paq.push(['setSiteId', '1']);
         var d = document,
             g = d.createElement('script'),
             s = d.getElementsByTagName('script')[0];
         g.async = true;
-        g.src = '//cdn.matomo.cloud/mchauveni.matomo.cloud/matomo.js';
+        g.src = u + 'matomo.js';
         s.parentNode.insertBefore(g, s);
     })();
 </script>
